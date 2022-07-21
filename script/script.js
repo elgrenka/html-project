e090ad65158b8820f561fcbe323df3a99ff525f3
elgrenka <bugigen@yandex.ru>
2022-07-21 19:35:01 +0400

    ready

--- script/script.js
+++ script/script.js
@@ -3,18 +3,24 @@
 }());
 
 let container = document.querySelector('.container');
-let music = document.querySelector('.music');
-let navMusic = document.querySelector('.nav-music');
+let music = document.querySelector('#music');
+let navMusic = document.querySelector('#nav-music');
 let boxMusic = document.querySelector('.box-music');
-let movies = document.querySelector('.movies');
-let navMovies = document.querySelector('.nav-movies');
+let movies = document.querySelector('#movies');
+let navMovies = document.querySelector('#nav-movies');
 let boxMovies = document.querySelector('.box-movies')
-let cartoons = document.querySelector('.cartoons');
-let navCartoons = document.querySelector('.nav-cartoons');
+let cartoons = document.querySelector('#cartoons');
+let navCartoons = document.querySelector('#nav-cartoons');
 let boxCartoons = document.querySelector('.box-cartoons');
-let pictures = document.querySelector('.pictures');
-let navPictures = document.querySelector('.nav-pictures');
+let pictures = document.querySelector('#pictures');
+let navPictures = document.querySelector('#nav-pictures');
 let boxPictures = document.querySelector('.box-pictures');
+let navComments = document.querySelector('#nav-comments');
+let boxComments = document.querySelector('.box-comments');
+let commentForm = document.querySelector('#commentForm');
+let commentName = document.querySelector('#commentName');
+let commentEmail = document.querySelector('#commentEmail');
+let commentText = document.querySelector('#commentText');
 
 music.addEventListener('click', () => {
   boxMusic.style.display = 'flex';
@@ -22,6 +28,7 @@ music.addEventListener('click', () => {
   boxMovies.style.display = 'none';
   boxCartoons.style.display = 'none';
   boxPictures.style.display = 'none';
+  boxComments.style.display = 'none';
 });
 
 navMusic.addEventListener('click', () => {
@@ -30,6 +37,7 @@ navMusic.addEventListener('click', () => {
   boxMovies.style.display = 'none';
   boxCartoons.style.display = 'none';
   boxPictures.style.display = 'none';
+  boxComments.style.display = 'none';
 });
 
 movies.addEventListener('click', () => {
@@ -38,6 +46,7 @@ movies.addEventListener('click', () => {
   boxMusic.style.display = 'none';
   boxCartoons.style.display = 'none';
   boxPictures.style.display = 'none';
+  boxComments.style.display = 'none';
 });
 
 navMovies.addEventListener('click', () => {
@@ -46,6 +55,7 @@ navMovies.addEventListener('click', () => {
   boxMusic.style.display = 'none';
   boxCartoons.style.display = 'none';
   boxPictures.style.display = 'none';
+  boxComments.style.display = 'none';
 });
 
 cartoons.addEventListener('click', () => {
@@ -54,6 +64,7 @@ cartoons.addEventListener('click', () => {
   container.style.display = 'none';
   boxMusic.style.display = 'none';
   boxPictures.style.display = 'none';
+  boxComments.style.display = 'none';
 });
 
 navCartoons.addEventListener('click', () => {
@@ -62,6 +73,7 @@ navCartoons.addEventListener('click', () => {
   container.style.display = 'none';
   boxMusic.style.display = 'none';
   boxPictures.style.display = 'none';
+  boxComments.style.display = 'none';
 });
 
 pictures.addEventListener('click', () => {
@@ -70,6 +82,7 @@ pictures.addEventListener('click', () => {
   container.style.display = 'none';
   boxMusic.style.display = 'none';
   boxCartoons.style.display = 'none';
+  boxComments.style.display = 'none';
 });
 
 navPictures.addEventListener('click', () => {
@@ -78,6 +91,45 @@ navPictures.addEventListener('click', () => {
   container.style.display = 'none';
   boxMusic.style.display = 'none';
   boxCartoons.style.display = 'none';
+  boxComments.style.display = 'none';
 });
 
+navComments.addEventListener('click', () => {
+  boxComments.style.display = 'block';
+  boxPictures.style.display = 'none';
+  boxMovies.style.display = 'none';
+  container.style.display = 'none';
+  boxMusic.style.display = 'none';
+  boxCartoons.style.display = 'none';
+})
+
+function addComment() {
+  commentForm.addEventListener('submit', (e) => {
+    if (commentName.value === "" || commentName.value.length < 3) {
+      window.alert("Your name must be at least three characters long");
+      commentName.focus();
+      return false;
+    }
+
+    if (commentEmail.value === "" ||
+      !((/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i).test(commentEmail.value))) {
+        window.alert("Enter valid email");
+        commentEmail.focus();
+        return false;
+    }
+
+    if (commentText.value === "" || commentText.value.length < 3) {
+      window.alert("Your comment must be at least three characters long");
+      commentText.focus();
+      return false;
+    }
+
+    alert('Your comment has been added');
+    e.preventDefault();
+    e.target.reset();
+  })
+  return true;
+};
+
+addComment();
 
