9b7f51fd6751e3be1646f2baa31f4131143c02f7
elgrenka <bugigen@yandex.ru>
2022-08-01 22:57:02 +0400

    add some puctures

--- script/script.js
+++ script/script.js
@@ -2,106 +2,110 @@
   document.querySelector('.footer').textContent += new Date().getFullYear();
 }());
 
-let container = document.querySelector('.container');
-let music = document.querySelector('#music');
-let navMusic = document.querySelector('#nav-music');
-let boxMusic = document.querySelector('.box-music');
-let movies = document.querySelector('#movies');
-let navMovies = document.querySelector('#nav-movies');
-let boxMovies = document.querySelector('.box-movies')
-let cartoons = document.querySelector('#cartoons');
-let navCartoons = document.querySelector('#nav-cartoons');
-let boxCartoons = document.querySelector('.box-cartoons');
-let pictures = document.querySelector('#pictures');
-let navPictures = document.querySelector('#nav-pictures');
-let boxPictures = document.querySelector('.box-pictures');
-let navComments = document.querySelector('#nav-comments');
-let boxComments = document.querySelector('.box-comments');
-let commentForm = document.querySelector('#commentForm');
-let commentName = document.querySelector('#commentName');
-let commentEmail = document.querySelector('#commentEmail');
-let commentText = document.querySelector('#commentText');
+function displayFlex() {
+  let container = document.querySelector('.container');
+  let music = document.querySelector('#music');
+  let navMusic = document.querySelector('#nav-music');
+  let boxMusic = document.querySelector('.box-music');
+  let movies = document.querySelector('#movies');
+  let navMovies = document.querySelector('#nav-movies');
+  let boxMovies = document.querySelector('.box-movies')
+  let cartoons = document.querySelector('#cartoons');
+  let navCartoons = document.querySelector('#nav-cartoons');
+  let boxCartoons = document.querySelector('.box-cartoons');
+  let pictures = document.querySelector('#pictures');
+  let navPictures = document.querySelector('#nav-pictures');
+  let boxPictures = document.querySelector('.box-pictures');
+  let navComments = document.querySelector('#nav-comments');
+  let boxComments = document.querySelector('.box-comments');
+  // let commentForm = document.querySelector('#commentForm');
+  // let commentName = document.querySelector('#commentName');
+  // let commentEmail = document.querySelector('#commentEmail');
+  // let commentText = document.querySelector('#commentText');
 
-music.addEventListener('click', () => {
-  boxMusic.style.display = 'flex';
-  container.style.display = 'none';
-  boxMovies.style.display = 'none';
-  boxCartoons.style.display = 'none';
-  boxPictures.style.display = 'none';
-  boxComments.style.display = 'none';
-});
+  music.addEventListener('click', () => {
+    boxMusic.style.display = 'flex';
+    container.style.display = 'none';
+    boxMovies.style.display = 'none';
+    boxCartoons.style.display = 'none';
+    boxPictures.style.display = 'none';
+    boxComments.style.display = 'none';
+  });
 
-navMusic.addEventListener('click', () => {
-  boxMusic.style.display = 'flex';
-  container.style.display = 'none';
-  boxMovies.style.display = 'none';
-  boxCartoons.style.display = 'none';
-  boxPictures.style.display = 'none';
-  boxComments.style.display = 'none';
-});
+  navMusic.addEventListener('click', () => {
+    boxMusic.style.display = 'flex';
+    container.style.display = 'none';
+    boxMovies.style.display = 'none';
+    boxCartoons.style.display = 'none';
+    boxPictures.style.display = 'none';
+    boxComments.style.display = 'none';
+  });
 
-movies.addEventListener('click', () => {
-  boxMovies.style.display = 'flex';
-  container.style.display = 'none';
-  boxMusic.style.display = 'none';
-  boxCartoons.style.display = 'none';
-  boxPictures.style.display = 'none';
-  boxComments.style.display = 'none';
-});
+  movies.addEventListener('click', () => {
+    boxMovies.style.display = 'flex';
+    container.style.display = 'none';
+    boxMusic.style.display = 'none';
+    boxCartoons.style.display = 'none';
+    boxPictures.style.display = 'none';
+    boxComments.style.display = 'none';
+  });
 
-navMovies.addEventListener('click', () => {
-  boxMovies.style.display = 'flex';
-  container.style.display = 'none';
-  boxMusic.style.display = 'none';
-  boxCartoons.style.display = 'none';
-  boxPictures.style.display = 'none';
-  boxComments.style.display = 'none';
-});
+  navMovies.addEventListener('click', () => {
+    boxMovies.style.display = 'flex';
+    container.style.display = 'none';
+    boxMusic.style.display = 'none';
+    boxCartoons.style.display = 'none';
+    boxPictures.style.display = 'none';
+    boxComments.style.display = 'none';
+  });
 
-cartoons.addEventListener('click', () => {
-  boxCartoons.style.display = 'flex';
-  boxMovies.style.display = 'none';
-  container.style.display = 'none';
-  boxMusic.style.display = 'none';
-  boxPictures.style.display = 'none';
-  boxComments.style.display = 'none';
-});
+  cartoons.addEventListener('click', () => {
+    boxCartoons.style.display = 'flex';
+    boxMovies.style.display = 'none';
+    container.style.display = 'none';
+    boxMusic.style.display = 'none';
+    boxPictures.style.display = 'none';
+    boxComments.style.display = 'none';
+  });
 
-navCartoons.addEventListener('click', () => {
-  boxCartoons.style.display = 'flex';
-  boxMovies.style.display = 'none';
-  container.style.display = 'none';
-  boxMusic.style.display = 'none';
-  boxPictures.style.display = 'none';
-  boxComments.style.display = 'none';
-});
+  navCartoons.addEventListener('click', () => {
+    boxCartoons.style.display = 'flex';
+    boxMovies.style.display = 'none';
+    container.style.display = 'none';
+    boxMusic.style.display = 'none';
+    boxPictures.style.display = 'none';
+    boxComments.style.display = 'none';
+  });
 
-pictures.addEventListener('click', () => {
-  boxPictures.style.display = 'flex';
-  boxMovies.style.display = 'none';
-  container.style.display = 'none';
-  boxMusic.style.display = 'none';
-  boxCartoons.style.display = 'none';
-  boxComments.style.display = 'none';
-});
+  pictures.addEventListener('click', () => {
+    boxPictures.style.display = 'flex';
+    boxMovies.style.display = 'none';
+    container.style.display = 'none';
+    boxMusic.style.display = 'none';
+    boxCartoons.style.display = 'none';
+    boxComments.style.display = 'none';
+  });
 
-navPictures.addEventListener('click', () => {
-  boxPictures.style.display = 'flex';
-  boxMovies.style.display = 'none';
-  container.style.display = 'none';
-  boxMusic.style.display = 'none';
-  boxCartoons.style.display = 'none';
-  boxComments.style.display = 'none';
-});
+  navPictures.addEventListener('click', () => {
+    boxPictures.style.display = 'flex';
+    boxMovies.style.display = 'none';
+    container.style.display = 'none';
+    boxMusic.style.display = 'none';
+    boxCartoons.style.display = 'none';
+    boxComments.style.display = 'none';
+  });
 
-navComments.addEventListener('click', () => {
-  boxComments.style.display = 'block';
-  boxPictures.style.display = 'none';
-  boxMovies.style.display = 'none';
-  container.style.display = 'none';
-  boxMusic.style.display = 'none';
-  boxCartoons.style.display = 'none';
-})
+  navComments.addEventListener('click', () => {
+    boxComments.style.display = 'block';
+    boxPictures.style.display = 'none';
+    boxMovies.style.display = 'none';
+    container.style.display = 'none';
+    boxMusic.style.display = 'none';
+    boxCartoons.style.display = 'none';
+  })
+};
+
+displayFlex();
 
 function addComment() {
   commentForm.addEventListener('submit', (e) => {
@@ -281,5 +285,52 @@ let chooseMult = () => {
 };
 
 chooseMult();
+
+let choosePhoto = () => {
+  let srcUrl = '';
+  let img = document.querySelector('#window-background img');
+  let photo_1 = document.querySelector('#photo_1');
+  let photo_2 = document.querySelector('#photo_2');
+  let photo_3 = document.querySelector('#photo_3');
+  let photo_4 = document.querySelector('#photo_4');
+  let photo_5 = document.querySelector('#photo_5');
+  let photo_6 = document.querySelector('#photo_6');
+  let photo_7 = document.querySelector('#photo_7');
+
+  photo_1.addEventListener('click', () => {
+    srcUrl = './pictures/Cats.jpg';
+    img.src = srcUrl;
+  });
+
+  song_2.addEventListener('click', () => {
+    srcUrl = './music/Galibri_Mavik_-_Federiko_Fellini.mp3';
+    audio.src = srcUrl;
+  });
+
+  song_3.addEventListener('click', () => {
+    srcUrl = './music/KHABIB_-_Yagoda_malinka.mp3';
+    audio.src = srcUrl;
+  });
+
+  song_4.addEventListener('click', () => {
+    srcUrl = './music/KVASHA_-_Zelenoglazoe_taksi.mp3';
+    audio.src = srcUrl;
+  });
+
+  song_5.addEventListener('click', () => {
+    srcUrl = './music/MBrother_-_Trebles.mp3';
+    audio.src = srcUrl;
+  });
+
+  song_6.addEventListener('click', () => {
+    srcUrl = './music/Scooter_-_4_AM.mp3';
+    audio.src = srcUrl;
+  });
+
+  song_7.addEventListener('click', () => {
+    srcUrl = './music/Sia_David_Guetta_-_Flames.mp3';
+    audio.src = srcUrl;
+  });
+};
 
 
